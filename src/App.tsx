c62caae8876ac7ce10eaf6d68d7016f78f09607e
anhduc109 <ducpham1098@gmail.com>
2020-03-24 13:56:50 +0200

    add error handling to front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import "./App.css";
 
@@ -11,19 +11,58 @@ function App() {
   const [quantity400g, setQuantity400g] = useState(0);
   const [quantity1000g, setQuantity1000g] = useState(0);
   const [minimumBox, setMinimumBox] = useState(0);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    // Error handling when changing quantity
+    if (quantity200g < 0 || quantity400g < 0 || quantity1000g < 0) {
+      setError("Quantity of Coffee Bags needs to be a positive number");
+    } else if (
+      quantity200g > 1000 ||
+      quantity400g > 1000 ||
+      quantity1000g > 1000
+    ) {
+      setError("Quantity of Coffee Bags needs to be smaller than 1000");
+    } else setError("");
+  }, [quantity200g, quantity400g, quantity1000g]);
 
   const handleQuantityInput = (
     evt: React.ChangeEvent<HTMLInputElement>,
     size: number
   ) => {
+    let value = evt.target.value === "" ? "0" : evt.target.value;
     if (size === 200) {
-      setQuantity200g(parseInt(evt.target.value));
+      setQuantity200g(parseInt(value));
     }
     if (size === 400) {
-      setQuantity400g(parseInt(evt.target.value));
+      setQuantity400g(parseInt(value));
     }
     if (size === 1000) {
-      setQuantity1000g(parseInt(evt.target.value));
+      setQuantity1000g(parseInt(value));
+    }
+  };
+
+  const handleDecreaseQuantity = (size: number) => {
+    if (size === 200) {
+      setQuantity200g(quantity200g - 1);
+    }
+    if (size === 400) {
+      setQuantity400g(quantity400g - 1);
+    }
+    if (size === 1000) {
+      setQuantity1000g(quantity1000g - 1);
+    }
+  };
+
+  const handleIncreaseQuantity = (size: number) => {
+    if (size === 200) {
+      setQuantity200g(quantity200g + 1);
+    }
+    if (size === 400) {
+      setQuantity400g(quantity400g + 1);
+    }
+    if (size === 1000) {
+      setQuantity1000g(quantity1000g + 1);
     }
   };
 
@@ -61,7 +100,12 @@ function App() {
                 src="https://www.slurp.coffee/wp-content/uploads/2015/12/Rogers-choco-900px-600x600.jpg"
               />
               <h3>200g</h3>
-              <div className="change-quantity-div">{"<"}</div>
+              <div
+                className="change-quantity-div no-select"
+                onClick={() => handleDecreaseQuantity(200)}
+              >
+                {"<"}
+              </div>
               <input
                 className="quantity-input"
                 min="0"
@@ -70,7 +114,12 @@ function App() {
                 value={quantity200g}
                 onChange={evt => handleQuantityInput(evt, 200)}
               />
-              <div className="change-quantity-div">{">"}</div>
+              <div
+                className="change-quantity-div no-select"
+                onClick={() => handleIncreaseQuantity(200)}
+              >
+                {">"}
+              </div>
             </div>
             <div className="coffee-detail">
               <img
@@ -79,16 +128,26 @@ function App() {
                 src="https://www.slurp.coffee/wp-content/uploads/2015/12/Rogers-choco-900px-600x600.jpg"
               />
               <h3>400g</h3>
-              <div className="change-quantity-div">{"<"}</div>
+              <div
+                className="change-quantity-div no-select"
+                onClick={() => handleDecreaseQuantity(400)}
+              >
+                {"<"}
+              </div>
               <input
-                className="quantity-input"
+                className="quantity-input no-select"
                 min="0"
                 max="1000"
                 type="number"
                 value={quantity400g}
                 onChange={evt => handleQuantityInput(evt, 400)}
               />
-              <div className="change-quantity-div">{">"}</div>
+              <div
+                className="change-quantity-div no-select"
+                onClick={() => handleIncreaseQuantity(400)}
+              >
+                {">"}
+              </div>
             </div>
             <div className="coffee-detail">
               <img
@@ -97,7 +156,12 @@ function App() {
                 src="https://www.slurp.coffee/wp-content/uploads/2015/12/Rogers-choco-900px-600x600.jpg"
               />
               <h3>1000g</h3>
-              <div className="change-quantity-div">{"<"}</div>
+              <div
+                className="change-quantity-div no-select"
+                onClick={() => handleDecreaseQuantity(1000)}
+              >
+                {"<"}
+              </div>
               <input
                 className="quantity-input"
                 min="0"
@@ -106,10 +170,15 @@ function App() {
                 value={quantity1000g}
                 onChange={evt => handleQuantityInput(evt, 1000)}
               />
-              <div className="change-quantity-div">{">"}</div>
+              <div
+                className="change-quantity-div no-select"
+                onClick={() => handleIncreaseQuantity(1000)}
+              >
+                {">"}
+              </div>
             </div>
           </div>
-          <br />
+          <h4 className="error-text">{error}</h4>
           <button type="submit" className="submit-btn">
             GENERATE
           </button>
